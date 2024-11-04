446590ca091e860cf7569cb649daf40c43d894f1
Denyme24 <namanraj24@outlook.com>
2024-11-04 17:27:49 +0530

    modified image paths

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import BizImg from "./assets/biz.png";
-import Dollar from "./assets/dollar.png";
-import Followers from "./assets/followers.png";
+import BizImg from "@/assets/biz.png";
+import Dollar from "@/assets/dollar.png";
+import Followers from "@/assets/followers.png";
 import Script from "next/script";
 
 export default function Home() {
